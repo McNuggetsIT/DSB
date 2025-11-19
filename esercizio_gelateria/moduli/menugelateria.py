130fc000fb1ab3cf3f3ef6d57e7ed683058d93c3
AntonioBrescia <antoniobrescia@rocketmail.com>
2025-11-19 16:30:33 +0100

    classe MenuGelateria

---
+++ esercizio_gelateria/moduli/menugelateria.py
@@ -0,0 +1,18 @@
+class MenuGelateria:
+    def __init__(self):
+        self._gusti = []
+
+    def aggiungi_gusto(self, gusto):
+        self._gusti.append(gusto)
+
+    def rimuovi_gusto(self, nome_gusto):
+        nuovi_gusti = []
+        for g in self._gusti:
+            if g.get_nome() != nome_gusto:
+                nuovi_gusti.append(g)
+        self._gusti = nuovi_gusti
+
+    def lista_gusti(self):
+        # Stampa la descrizione di tutti i gusti
+        for g in self._gusti:
+            print(g.descrizione())
